c02cadf1428dae423228eb36c7776dbd7dae5326
sarah19990 <sara1999200407@gmail.com>
2023-05-04 16:28:51 +0100

    clothes shopping for womens,mens and kids clothing

---
+++ shop/store/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from store.models import Category, Customer, Products, Order
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Products, AdminProduct)
+admin.site.register(Order)
